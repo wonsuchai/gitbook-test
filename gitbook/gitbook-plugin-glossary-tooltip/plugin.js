67092d64ea11a5695a9e2a94257f0a2e15cd2945
Francesco Mariani <deploy@travis-ci.org>
2020-04-13 22:45:28 +0000

    Deploy francesco-mariani/gitbook-test to github.com/francesco-mariani/gitbook-test.git:gh-pages

---
+++ gitbook/gitbook-plugin-glossary-tooltip/plugin.js
@@ -0,0 +1,44 @@
+require([
+    'jquery',
+    'gitbook',
+], function ($, gitbook) {
+    var setup = function () {
+        var opts = {
+            parseGlossaryItems: true,
+            trigger: 'hover',
+            placement: 'top',
+        };
+        var selectors = '.gbgt-tooltip';
+        var glossarySelector = '.glossary-term';
+        try {
+            opts = $.extend(opts, gitbook.state.config.pluginsConfig['glossary-tooltip']);
+        } catch (e) {}
+
+        if (opts.parseGlossaryItems) {
+            selectors = selectors + ',' + glossarySelector;
+        }
+        
+        var $selectors = $(selectors);
+        $selectors.each(function() {
+            var $this = $(this);
+            var title = $this.attr('title');
+            $this.removeAttr('title');
+            const instance = new Tooltip(this, {
+                title: title,
+                html: true,
+                trigger: /hover|click|focus/.test(opts.trigger) ? opts.trigger : 'hover',
+                placement: /top|left|bottom|right/.test(opts.placement) ? opts.placement : 'top',
+                modifiers: {
+                    flip: {
+                        behavior: ['left', 'bottom', 'top']
+                    },
+                    preventOverflow: {
+                        boundariesElement: $('div.page-inner'),
+                    },
+                },
+            });
+        })
+    };
+
+    gitbook.events.bind('page.change', setup);
+});
